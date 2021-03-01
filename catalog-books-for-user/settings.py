e6b692d22b0ce16f9094e6ac9e875d375dc72820
vova_savin <volodimirsavin56@gmail.com>
2021-03-01 22:17:59 +0200

    Нечего коммитить

--- catalog-books-for-user/settings.py
+++ catalog-books-for-user/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,4 +130,3 @@ DATABASES['default'].update(db_from_env)
 # Уменьшение размера статических файлов
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
