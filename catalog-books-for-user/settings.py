6fe62a309b7e10bce250434280a2b1610b23bf25
vova_savin <volodimirsavin56@gmail.com>
2021-03-02 14:12:35 +0200

    Нечего коммитить

--- catalog-books-for-user/settings.py
+++ catalog-books-for-user/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 't23xjuwpzgd@czimim2ss!q49m_g)+
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != False
-export = DJANGO_DEBUG = False
+
 
 ALLOWED_HOSTS = ['tranquil-temple-96943.herokuapp.com', '127.0.0.1']
 
